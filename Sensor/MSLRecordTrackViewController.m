6ae136e4ad577a52146a8ed7bbd1d3f2dccee6bb
Sami Purmonen <purmonen@kth.se>
2014-02-18 17:25:33 +0100

    Added UI

--- Sensor/MSLRecordTrackViewController.m
+++ Sensor/MSLRecordTrackViewController.m
@@ -7,32 +7,86 @@
 //
 
 #import "MSLRecordTrackViewController.h"
+#import "MSLAccelerometerHandler.h"
 
 @interface MSLRecordTrackViewController ()
+
+
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property MSLAccelerometerHandler *handler;
+@property NSTimer *timer;
+@property NSDate *startTime;
+@property (weak, nonatomic) IBOutlet UILabel *accelerationDataLabel;
+@property BOOL isRunning;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+- (IBAction)start:(UIButton *)sender;
 
 @end
 
 @implementation MSLRecordTrackViewController
 
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    self.isRunning = NO;
+    self.startTime = [NSDate date];
+    self.handler = [MSLAccelerometerHandler initWithInterval:2];
 }
+
+
+- (void)updateTimeLabel:(NSTimer *)timer {
+    int interval = -(int)[self.startTime timeIntervalSinceNow];
+    int seconds = interval % 60;
+    int minutes = (interval / 60) % 60;
+    int hour = interval / (60*60);
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"%02d:%02d:%02d", hour, minutes, seconds];
+    
+    NSMutableArray *data = [self.handler getData];
+
+    if (![data count]) {
+        NSLog(@"NO DATA");
+        return;
+    }
+    CMAccelerometerData *accData = (CMAccelerometerData *)[data lastObject];
+    CMAcceleration acc = accData.acceleration;
+    
+    self.accelerationDataLabel.text = [NSString stringWithFormat:@"X: %.3f, Y: %.3f, Z: %.3f", acc.x, acc.y, acc.z];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)start:(UIButton *)sender {
+    if (!self.isRunning) {
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateTimeLabel:) userInfo:nil repeats:YES];
+        self.startTime = [NSDate date];
+        [self.timer fire];
+        [self.handler start];
+        [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+    } else {
+    	[self.timer invalidate];
+        self.timer = nil;
+    	[self.handler stop];
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    self.isRunning = !self.isRunning;
+}
+
 
 @end
